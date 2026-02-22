befed41237a9d4f9478a23b73a359b049daa5ad5
Pavly Adel Halim Gouda <bavlyadel929@gmail.com>
2026-02-22 17:41:14 +0200

    Update Header.jsx

--- Frontend-ReactJS/src/components/Header/Header.jsx
+++ Frontend-ReactJS/src/components/Header/Header.jsx
@@ -8,34 +8,83 @@ import "./header.css"
  * 
  * @returns {React.ReactElement}
  */
+
+
 function Header() {
+  const user = useSelector((state) => state.user);
 
- const user = useSelector((state) => state.user);
+  return (
+    <nav className="navbar">
+      <div className="nav-container">
+        
+        {/* Left Side */}
+        <div className="nav-left">
+          <NavLink to="/" className="logo">
+            StudyBuddy
+          </NavLink>
 
- return (
-  <nav className="NavBar" role="navigation" aria-labelledby="firstLabel" aria-label="Primary">
-   <NavLink to="/" className="NavBar-AppName">
-    App Name
-   </NavLink>
-   <div>
-    {
-     (!user || !user.loggedIn) && (
-      <ul className="NavBar-Items">
-       <li><NavLink to="/login">Login</NavLink></li>
-       <li><NavLink to="/signup">Signup</NavLink></li>
-      </ul>
-     )
-    }
-    {
-     (user && user.loggedIn) && (
-      <ul className="NavBar-Items">
-       <li><NavLink to="/dashboard">Dashboard</NavLink></li>
-      </ul>
-     )
-    }
-   </div>
-  </nav>
- );
+          <ul className="nav-main-links">
+            <li>
+              <NavLink to="/find-expert" className="nav-link">
+                Find Expert
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/add-material" className="nav-link">
+                Add Material
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Bootcamp" className="nav-link">
+                Boot Camp
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/Work" className="nav-link">
+                Work
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="Offer" className="nav-link">
+                Offer
+              </NavLink>
+            </li>
+            <li>
+              More
+            </li>
+          </ul>
+        </div>
+
+        {/* Right Side */}
+        <div className="nav-right">
+          {!user?.loggedIn && (
+            <>
+              <NavLink to="/login" className="nav-link">
+                Login
+              </NavLink>
+
+              <NavLink to="/signup" className="btn-primary">
+                JOIN US
+              </NavLink>
+            </>
+          )}
+
+          {user?.loggedIn && (
+            <>
+              <NavLink to="/dashboard" className="nav-link">
+                Dashboard
+              </NavLink>
+
+              <button className="btn-outline">
+                Logout
+              </button>
+            </>
+          )}
+        </div>
+
+      </div>
+    </nav>
+  );
 }
 
 export default Header;
