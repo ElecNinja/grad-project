b4c09cd0f99383d4f44c2b8b6f6df3402b5c5f45
Pavly Adel Halim Gouda <bavlyadel929@gmail.com>
2026-02-23 08:30:06 +0200

    Update Login.jsx

--- Frontend-ReactJS/src/pages/Login/Login.jsx
+++ Frontend-ReactJS/src/pages/Login/Login.jsx
@@ -1,18 +1,19 @@
 import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Link } from 'react-router-dom'; // if using react-router for internal routing
+import { useNavigate, Link } from 'react-router-dom';
 import { useDispatch } from "react-redux";
 import { INPUT_LENGTH } from "../../utils/constants.js";
 import { setLoader } from '../../redux/loaderSlice.js';
 import useIsComponentMounted from "../../hooks/useIsComponentMounted.js";
 import { loginUser } from '../../apis/handlers/loginUser.js';
 import ErrorMessage from '../../components/ErrorMessage/ErrorMessage.jsx';
-
+import "./Login.css";
 /**
  * Component returns login page.
  * 
  * @returns {React.ReactElement}
  */
+
+
 function Login() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
@@ -26,105 +27,97 @@ function Login() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    const requestData = {
-      email: email,
-      password: password,
-      remember: remember
-    }
+    const requestData = { email, password, remember };
 
     dispatch(setLoader(true));
 
     loginUser(requestData)
       .then(res => {
         if (isComponentMounted) {
-          if (res.response) { navigate('/dashboard'); }
-          else { setError(res.message); }
+          if (res.response) navigate('/dashboard');
+          else setError(res.message);
         }
       })
-      .catch(error => { console.error("Error in login function.", error); })
-      .finally(() => { dispatch(setLoader(false)); })
-
+      .catch(error => console.error("Error in login function.", error))
+      .finally(() => dispatch(setLoader(false)));
   };
 
   return (
-    <section className="Main-Section">
-      {/* Metadata */}
-      <title>Login</title>
+    <div className="login-wrapper">
+      <div className="login-card">
 
-      {/* Component */}
-      <div className="Main-Form-Container">
-        <h1>Login</h1>
-        <br />
-        <br />
-        <p>Welcome back!</p>
-        <br />
-        <br />
+        <div className="top-bar">
+          <div className="logo">StudyBuddy</div>
+        </div>
 
-        {error && (
-          <ErrorMessage message={error} />
-        )}
+        <div className="login-content">
 
-        <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor="email">
-              <b>Email *</b>
-            </label>
-            <input
-              autoComplete="email"
-              id="email"
-              name="email"
-              type="text"
-              placeholder="Enter Email"
-              minLength={INPUT_LENGTH.email.minValue}
-              maxLength={INPUT_LENGTH.email.maxValue}
-              required
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
+          <h2>Login to your account</h2>
 
-          <div>
-            <label htmlFor="password">
-              <b>Password *</b>
-            </label>
-            <input
-              autoComplete="current-password"
-              id="password"
-              name="password"
-              type="password"
-              placeholder="Enter Password"
-              minLength={INPUT_LENGTH.password.minValue}
-              maxLength={INPUT_LENGTH.password.maxValue}
-              required
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-            />
-          </div>
+          {error && <ErrorMessage message={error} />}
 
-          <div>
-            <label className="Main-Form-Checkbox">
+          <form onSubmit={handleSubmit}>
+
+            <div className="form-group">
+              <label>Email</label>
               <input
-                type="checkbox"
-                name="remember"
-                checked={remember}
-                onChange={(e) => setRemember(e.target.checked)}
+                autoComplete="email"
+                type="email"
+                placeholder="Enter your email"
+                minLength={INPUT_LENGTH.email.minValue}
+                maxLength={INPUT_LENGTH.email.maxValue}
+                required
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
-              Remember me
-            </label>
-          </div>
+            </div>
 
-          <div>
-            <button type="submit">Login</button>
-          </div>
-        </form>
+            <div className="form-group">
+              <div className="password-row">
+                <label>Password</label>
+                <span className="forgot">Forgot?</span>
+              </div>
 
-        <br />
-        <br />
-        <p>
-          Don't have an account? <Link to="/signup">Signup</Link>
-        </p>
+              <input
+                autoComplete="current-password"
+                type="password"
+                placeholder="Enter your password"
+                minLength={INPUT_LENGTH.password.minValue}
+                maxLength={INPUT_LENGTH.password.maxValue}
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+
+            <div className="checkbox-row">
+              <label>
+                <input
+                  type="checkbox"
+                  checked={remember}
+                  onChange={(e) => setRemember(e.target.checked)}
+                />
+                Remember me
+              </label>
+            </div>
+
+            <button className="primary-btn" type="submit">
+              Login now
+            </button>
+
+            <button type="button" className="google-btn">
+              Continue with Google
+            </button>
+
+          </form>
+
+          <p className="signup-text">
+            Don’t have an account? <Link to="/signup">Sign Up</Link>
+          </p>
+
+        </div>
       </div>
-    </section>
+    </div>
   );
 }
 
